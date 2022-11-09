22f103b3672e0aa9ca2c44b89f55a5f1fa482871
Nikita <nikita_isaichenk@ro.ru>
2022-11-10 02:41:38 +0700

    .

--- LAB_1/LAB_1/Actions.h
+++ LAB_1/LAB_1/Actions.h
@@ -1,13 +1,56 @@
 #pragma once
+/*****************************************************************//**
+ * \file   Actions.h
+ * \brief  
+ * 
+ * \author nikit
+ * \date   November 2022
+ *********************************************************************/
 #include "DynamicArray.h"
-
+/**
+ * Создает динамический массив.
+ */
 void CreatDynamicArray(DynamicArray* dynamicArray);
+
+/**
+ * Добавляет элемент в конец динамического массива.
+ */
 void AddElement(DynamicArray* dynamicArray);
+
+/**
+ * Удаляет элемент из динамического массива по индексу.
+ */
 void RemoveElement(DynamicArray* dynamicArray);
+
+/**
+ * Вставляет в динамический массив элемент по указанному индексу.
+ */
 void InsertElement(DynamicArray* dynamicArray);
+
+/**
+ * Сортирует динамический массив.
+ */
 void SortArray(DynamicArray* dynamicArray);
+
+/**
+ * Осуществляет линейный поиск элемента по динамическому массиву.
+ */
 void LinearSearchElement(DynamicArray* dynamicArray);
+
+/**
+ * Осуществляет бинарный поиск элемента по динамическому массиву.
+ */
 void BinarySearchElement(DynamicArray* dynamicArray);
+
+/**
+ * Пересоздает динамический массив.
+ */
 void RecreatDynamicArray(DynamicArray* dynamicArray);
+
+/**
+ * Выводит динамический массив.
+ */
 void ShowArray(DynamicArray* dynamicArray);
+
+
 
